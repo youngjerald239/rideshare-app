c732614af49d88cb43d82fab4afb693b4fafa007
youngjerald239 <youngjerald239@gmail.com>
2022-04-28 10:18:39 -0500

    added tailwind styled components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import tw from 'tailwind-styled-components/dist/tailwind'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.map}>MAP</div>
-      <div className={styles.start}>Start</div>
-    </div>
+    <Wrapper>
+      <Map>Map</Map>
+      <ActionItems>Start</ActionItems>
+    </Wrapper>
   )
 }
+
+const Wrapper = tw.div``
+
+const ActionItems = tw.div``
