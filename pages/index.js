dcda28eef5479dbc4af911f414fbbf17f160baed
youngjerald239 <youngjerald239@gmail.com>
2022-04-29 00:04:28 -0500

    created map component and rendered on app

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import tw from 'tailwind-styled-components/dist/tailwind'
 import mapboxgl from '!mapbox-gl'
 
-mapboxgl.accessToken = 'pk.eyJ1IjoieW91bmdqZXJhbGQyMzkiLCJhIjoiY2t5Y2FtenBnMG5jczJwbXJ4c3QxbGFmeSJ9.8AsktT65tLtjWUniauoLqw'
+mapboxgl.accessToken = 
+'pk.eyJ1IjoieW91bmdqZXJhbGQyMzkiLCJhIjoiY2t5Y2FtenBnMG5jczJwbXJ4c3QxbGFmeSJ9.8AsktT65tLtjWUniauoLqw'
 
 export default function Home() {
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-99.29011, 39.39172],
+      zoom: 3
+    });
+  })
+
+  // const map = new mapboxgl.Map({
+  //   container: 'map',
+  //   style:'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8t',
+  //   center: ,
+  //   zoom: ,
+  // })
+
+
   return (
     <Wrapper>
-      <Map>Map</Map>
+      <Map id = "map"></Map>
       <ActionItems>Start</ActionItems>
     </Wrapper>
   )
