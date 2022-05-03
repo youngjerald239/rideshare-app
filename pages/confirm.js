fe274af855664ccd55fe690d8b8b48c1c7addf05
youngjerald239 <youngjerald239@gmail.com>
2022-05-03 13:33:17 -0500

    added geocoding api data

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import {useEffect} from 'react'
 import tw from 'tailwind-styled-components/dist/tailwind'
 import Map from './components/Map'
 
-const confirm = () => {
+const Confirm = () => {
+
+    const getCoordinates = () => {
+        const location = "Santa Monica"
+        //fetch geocoding map api data
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?` +
+         new URLSearchParams({
+             access_token: "pk.eyJ1IjoieW91bmdqZXJhbGQyMzkiLCJhIjoiY2t5Y2FtenBnMG5jczJwbXJ4c3QxbGFmeSJ9.8AsktT65tLtjWUniauoLqw",
+             limit: 1
+         })
+        )
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+        })
+        //access_token
+    }
+
+    useEffect(() => {
+        getCoordinates()
+    }, [])
+
   return (
     <Wrapper>
         <Map />
@@ -15,7 +36,7 @@ const confirm = () => {
   )
 }
 
-export default confirm
+export default Confirm
 
 const Wrapper = tw.div`
 flex h-screen flex-col
