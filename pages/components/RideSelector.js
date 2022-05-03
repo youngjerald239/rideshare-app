ee970be93eb7c341960d332fc6538ab8b2d75a9b
youngjerald239 <youngjerald239@gmail.com>
2022-05-03 18:01:16 -0500

    added car div and list

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -4,13 +4,28 @@ import tw from 'tailwind-styled-components/dist/tailwind'
 const RideSelector = () => {
   return (
     <Wrapper>
-        Ride Selector
+        <Title>Choose a ride, or swipe up for more</Title>
+        <CarList>
+            <Car>
+                <CarImage src = "" />
+            </Car>
+        </CarList>
     </Wrapper>
   )
 }
 
 export default RideSelector
 
+const CarList = tw.div``
+
+const Car = tw.div``
+
+const CarImage = tw.img``
+
+const Title = tw.div`
+text-green-300 text-center text-xs py-2 border-b
+`
+
 const Wrapper = tw.div`
-flex flex-1 
+flex-1 
 `
