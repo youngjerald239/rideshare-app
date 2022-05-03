1a154b7d8e7fee4af32122ad0d5b565bd71d2d56
youngjerald239 <youngjerald239@gmail.com>
2022-05-03 15:08:11 -0500

    added props for dropoff location map marker

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -20,6 +20,10 @@ const Map = (props) => {
     if(props.pickupCoordinates){
         addToMap(map, props.pickupCoordinates)
     }
+
+    if(props.dropoffCoordinates){
+        addToMap(map, props.dropoffCoordinates)
+    }
   }, [props.pickupCoordinates, props.dropoffCoordinates])
 
   const addToMap = (map, coordinates) => {
