a3689fd9adaf2546d6de6d0878a8cc149736004f
youngjerald239 <youngjerald239@gmail.com>
2022-05-03 15:18:40 -0500

    added map fitBounds to use auto zoom

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -6,6 +6,7 @@ import { useEffect } from 'react'
 mapboxgl.accessToken = 
 'pk.eyJ1IjoieW91bmdqZXJhbGQyMzkiLCJhIjoiY2t5Y2FtenBnMG5jczJwbXJ4c3QxbGFmeSJ9.8AsktT65tLtjWUniauoLqw'
 
+//The Map style will display
 const Map = (props) => {
     console.log(props)
 
@@ -16,16 +17,26 @@ const Map = (props) => {
        center: [-99.29011, 39.39172],
        zoom: 3
      })
-
+     //Props for pickup map marker locations to appear on map
     if(props.pickupCoordinates){
         addToMap(map, props.pickupCoordinates)
     }
-
+    //Props for dropoff map marker locations to appear on map
     if(props.dropoffCoordinates){
         addToMap(map, props.dropoffCoordinates)
     }
+    //Props to auto zoom between both coordinates
+    if(props.pickupCoordinates && props.dropoffCoordinates){
+      map.fitBounds([
+          props.pickupCoordinates,
+          props.dropoffCoordinates
+      ], {
+          padding: 60
+      })  
+    }
   }, [props.pickupCoordinates, props.dropoffCoordinates])
 
+  //Adds the marker icon to the map
   const addToMap = (map, coordinates) => {
     const marker1 = new mapboxgl.Marker()
     .setLngLat(coordinates)
