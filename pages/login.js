c4b6fb07b09f5897227333d2e3ac1b1e6d8ba8ea
youngjerald239 <youngjerald239@gmail.com>
2022-05-04 12:28:25 -0500

    created login with animated image

--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,9 @@ import tw from 'tailwind-styled-components/dist/tailwind'
 const Login = () => {
   return (
     <Wrapper>
+    <PiedmontLogo src= "https://res.cloudinary.com/dbgpqig0z/image/upload/v1642268523/Screenshot_153_vjsmta.png" />
+        <Title>Log in to access your account</Title>
+        <HeadImage src='https://cdn-icons-gif.flaticon.com/7308/7308525.gif'/>
         <SignInButton>
             Sign in with Google
         </SignInButton>
@@ -13,6 +16,22 @@ const Login = () => {
 
 export default Login
 
-const Wrapper = tw.div``
+const Wrapper = tw.div`
+flex flex-col h-screen bg-gray-600 p-4
+`
 
-const SignInButton = tw.button``
+const SignInButton = tw.button`
+bg-black text-green-300 text-center py-4 mt-8 self-center w-full
+`
+
+const PiedmontLogo = tw.img`
+h-20 w-auto object-contain self-start rounded-lg
+`
+
+const Title = tw.div`
+text-5xl pt-4 text-green-200
+`
+
+const HeadImage = tw.img`
+object-contain w-full bg-black-900
+`
