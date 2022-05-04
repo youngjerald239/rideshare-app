7187c7e92b1f2c282e427bbcdc2cb8b65f7a1e9a
youngjerald239 <youngjerald239@gmail.com>
2022-05-03 23:06:14 -0500

    added car list array

---
+++ pages/data/carList.js
@@ -0,0 +1,27 @@
+export const carList = [
+ {
+  imgUrl: 'https://cdn-icons-png.flaticon.com/128/1085/1085850.png',
+  service: 'Ride(s)',
+  multiplier: 1
+ },
+ {
+  imgUrl: '',
+  service: 'Ride(L)',
+  multiplier: 1.5
+},
+{
+  imgUrl: '',
+  service: 'Ride(XL)',
+  multiplier: 2
+},
+{
+  imgUrl: '',
+  service: 'Ride(Truck)',
+  multiplier: 1.2
+},
+{
+  imgUrl: '',
+  service: 'Ride(SUV)',
+  multiplier: 2.8
+},
+]
